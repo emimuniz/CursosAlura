138bd390a2dae0df2a51f6da731b4976b073ffb9
Emillyn <emillyn_muniz@hotmail.com>
2021-06-14 09:10:31 -0300

    Ajustando Service

--- Kotlin/forum/src/main/kotlin/com/alura/demo/service/TopicoService.kt
+++ Kotlin/forum/src/main/kotlin/com/alura/demo/service/TopicoService.kt
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService {
-    fun listar() :   List<Topico>{
+class TopicoService(private var topicos: List<Topico>) {
+
+    init{
         val topico = Topico(
             id = 1,
             titulo = "Duvida Kotlin",
@@ -24,7 +25,26 @@ class TopicoService {
                 email = "emillyn_muniz@hotmail.com"
             )
         )
-        return Arrays.asList(topico, topico, topico)
+        val topico2 = Topico(
+            id = 2,
+            titulo = "Duvida Kotlin 2",
+            mensagem = "Variaveis no Kotlin 2",
+            curso = Curso(
+                id = 1,
+                nome = "Kotlin",
+                categoria = "Programação"
+            ),
+            autor = Usuario(
+                id = 1,
+                nome = "Emillyn Silva",
+                email = "emillyn_muniz@hotmail.com"
+            )
+        )
+        topicos =  Arrays.asList(topico, topico2, topico)
+    }
+
+    fun listar() :   List<Topico>{
+       return topicos
     }
 
 }
