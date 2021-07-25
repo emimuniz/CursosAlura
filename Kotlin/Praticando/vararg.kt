754117d77b176ba0d5bfcfe9238a96b9f0d90ca6
Emillyn Muniz <emillyn_muniz@hotmail.com>
2021-07-25 16:42:22 -0300

    Create vararg.kt

---
+++ Kotlin/Praticando/vararg.kt
@@ -0,0 +1,11 @@
+fun sayHello(greeting: String, vararg itemToGreet: String) {
+    itemToGreet.forEach{ itemToGreet -> 
+        println("$greeting $itemToGreet")
+    }
+}
+
+fun main(){
+    val interestingThings = arrayOf("Kotlin", "Programming", "Comic Books")
+	sayHello("Hi", "Kotlin", "Programming", "Comic Books")
+    sayHello("Hi", *interestingThings)
+}
