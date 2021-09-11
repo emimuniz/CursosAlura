c4828daff5a02b7707e751d0f793e75cacd1640e
Emillyn <emillyn_muniz@hotmail.com>
2021-09-10 22:00:19 -0300

    Autorizando o acesso ao Usuario

--- Kotlin/forum/src/main/kotlin/com/alura/demo/config/SecurityConfiguration.kt
+++ Kotlin/forum/src/main/kotlin/com/alura/demo/config/SecurityConfiguration.kt
@@ -18,6 +18,7 @@ class SecurityConfiguration(
     override fun configure(http: HttpSecurity?) {
         http?.
         authorizeRequests()?.
+        antMatchers("/topicos")?.hasAuthority("LEITURA_ESCRITA")?.
         anyRequest()?.
         authenticated()?.and()?.
         sessionManagement()?.sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
